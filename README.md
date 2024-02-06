f297a8db86d1e741c7507a0f85d93f6fad6260a9
iamshivamp <shivvdog623@gmail.com>
2024-02-05 22:46:02 -0500

    added requirements.txt | refactored run.sh

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ### Descripition
 
 This is an api that is using data from movies such as Title, Actors, Directors, etc.
+
+### Psycopg2 for M2
+
+`M2 Mac` needs `psycopg2-binary` instead of the regular `psycopg2`.
--- run.sh
+++ run.sh
@@ -3,5 +3,8 @@
 echo "creating virtual environment..."
 python3 -m venv flaskenv
 
-echo "activating virtual environment..."
-source flaskenv/bin/activate
+echo "printing command to activate the environment..."
+echo ". flaskenv/bin/activate"
+
+echo "install dependencies using the command below..."
+echo "pip3 install -r requirements.txt"
