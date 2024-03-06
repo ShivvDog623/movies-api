7c485f0c80f08785a3410593092f6140bf1d4cb8
iamshivamp <shivvdog623@gmail.com>
2024-03-05 23:33:11 -0500

    changed folder name | refactored service file | took away jsonify in movie-genres bp

--- blueprints/movie_genres/blueprints.py
+++ blueprints/movie_genres/blueprints.py
@@ -5,7 +5,7 @@ Movie_Genre Blueprint. Serves movie_actor table related endpoints.
 from flask import Blueprint, jsonify, request
 from app import db
 from doQuery import doQuery
-from .service import get_all_movie_genres, get_by_id
+from .service import get_all_movie_genres, get_id
 
 movie_genre_blueprint = Blueprint('movie_genre', __name__, url_prefix='/movie_genre')
 
@@ -16,13 +16,13 @@ def get_all():
     GET: returns movie_genre table data
     """
     result = get_all_movie_genres()
-    return jsonify(result)
+    return result
 
 
 @movie_genre_blueprint.route('/<int:id>', methods=['GET'])
-def get_id(id):
+def get_by_id(id):
     """
     GET: returns movie_genre table data by genre_id
     """
-    result = get_by_id(id)
-    return jsonify (result)
+    result = get_id(id)
+    return result
