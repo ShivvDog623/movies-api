94163e1747a449f5cfcb48854258b170a198b2a4
iamshivamp <shivvdog623@gmail.com>
2024-03-18 16:06:17 -0400

    added pydantic to genres bp | refactored service file | refactored app

--- blueprints/genres/blueprints.py
+++ blueprints/genres/blueprints.py
@@ -1,49 +1,107 @@
 """
 Genre Blueprint. Serves all genre table related endpoints
 """
+from typing import Optional
 from flask import Blueprint, request
-from .service import get_all, get_id, create_genre, update_by_id, delete_by_id
+from pydantic import BaseModel
+from flask_pydantic import validate
+
+from .service import(
+    get_all, 
+    get_id, 
+    create_genre,
+    update_by_id, 
+    delete_by_id)
 
 genre_blueprint = Blueprint('genre', __name__, url_prefix='/genre')
 
+class GenreItem(BaseModel):
+    """genre item data model"""
+    genre: str
+
+class GenreDataModel(BaseModel):
+    """genre data model"""
+    genre_id: int
+    genre: str
+
+
+class MessageModel(BaseModel):
+    """generic message model"""
+    message: str
+
+class GenreResponseModel(BaseModel):
+    """genre response data model"""
+    status: int | str
+    data: list[GenreDataModel | MessageModel]
+
+class FieldValueModel(BaseModel):
+    """genre field and value model"""
+    field: str
+    value: str | int | float | bool
+
+class PatchModel(BaseModel):
+    """genre search and patch model"""
+    fields: list[FieldValueModel]
+
+class ValueModel(BaseModel):
+    """genre values model"""
+    value: str | int | float | bool
+
+class PostModel(BaseModel):
+    """new genre model"""
+    status: int | str
+
+
+
 @genre_blueprint.route('/all', methods=['GET'])
+@validate()
 def get():
     """
     GET: returns all genres table data
     """
     result = get_all()
-    return result
+    return GenreResponseModel(status=result["status"], data=result["data"])
 
 @genre_blueprint.route('/<int:id>', methods=['GET'])
+@validate()
 def get_by_id(id):
     """
     GET: returns genre by id
     """
     result = get_id(id)
-    return result
+    return GenreResponseModel(status=result["status"], data=result["data"])
 
 @genre_blueprint.route('/create', methods= ['POST'])
+@validate(body=GenreItem)
 def create():
     """
     POST: creates genre in genre table
     """
     data = request.get_json()
     result = create_genre(data)
-    return result
-
+    if (result["status"] == 200):
+        status = 201
+    else:
+        status = result["status"]
+    return PostModel(status=status)
 
 @genre_blueprint.route('/<int:id>', methods=['PUT'])
+@validate(body=GenreItem)
 def update(id):
     """
     PUT: updates genre by id
     """
-    result = update_by_id(id)
-    return result
+    data = request.get_json()
+    result = update_by_id(id, data)
+    if (result["status"] == 204):
+        return MessageModel(status=result["status"], message=result["message"])
+    return GenreResponseModel(status=result["status"], data=result["data"])
 
 @genre_blueprint.route('/<int:id>', methods= ['DELETE'])
+@validate()
 def delete(id):
     """
     DELETE: Deletes genre by id (be careful).
     """
     result = delete_by_id(id)
-    return result
+    return GenreResponseModel(status=result["status"], data=result["data"])
--- blueprints/genres/service.py
+++ blueprints/genres/service.py
@@ -32,12 +32,16 @@ def create_genre(data):
 
 
 
-def update_by_id(id):
+def update_by_id(id, data):
     """
     SERVICE: Update genre by id
     """
-    sql = "UPDATE movies.genre SET genre='CrazyKillerBee' WHERE genre_id =%s RETURNING genre"
-    params = [id]
+    genre = data.get('genre')
+    sql = """UPDATE movies.genre 
+    SET genre= %s
+    WHERE genre_id = %s 
+    RETURNING *"""
+    params = [genre ,id]
     result = doQuery(sql, params)
     return result
 
@@ -46,7 +50,7 @@ def delete_by_id(id):
     """
     SERVICE: Delete genre by id (Be careful)
     """
-    sql = 'DELETE FROM movies.genre WHERE genre_id = %s RETURNING genre_id'
+    sql = 'DELETE FROM movies.genre WHERE genre_id = %s RETURNING *'
     params = [id]
     result = doQuery(sql, params)
     return result
