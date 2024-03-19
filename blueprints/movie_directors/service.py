3a57377e7ad37e5d8554db178e0d6276086c91ed
iamshivamp <shivvdog623@gmail.com>
2024-03-19 01:35:07 -0400

    refactored bp and service file

--- blueprints/movie_directors/service.py
+++ blueprints/movie_directors/service.py
@@ -14,7 +14,14 @@ def get_id(id):
     """
     SERVICE: returns movie_director by director id
     """
-    sql = "SELECT * FROM movies.movie_director WHERE director_id = %s"
+    sql = """
+            SELECT * FROM movies.movie_director
+            INNER JOIN movies.director
+            ON movies.movie_director.director_id = movies.director.director_id
+            INNER JOIN movies.movie
+            ON movies.movie.movie_id = movies.movie_director.movie_id
+            WHERE movies.director.director_id = %s
+            """
     params = [id]
     result = doQuery(sql, params)
     return result
